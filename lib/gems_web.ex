c2ae8ddbe1c9664d9925557b14980695aa7e82e7
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2024-11-09 12:56:00 -0300

    WIP

--- lib/gems_web.ex
+++ lib/gems_web.ex
@@ -90,6 +90,9 @@ defmodule GEMSWeb do
       # Shortcut for generating JS commands
       alias Phoenix.LiveView.JS
 
+      # Alias for the UI component helpers
+      alias GEMSWeb.Components, as: UI
+
       # Routes generation with the ~p sigil
       unquote(verified_routes())
     end
