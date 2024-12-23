bcefdf01ebaf83c2c7c1ae2cf46e81b22b7abd40
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2024-12-23 15:27:44 -0300

    Change format

--- lib/gems_web/live/user_login_live.ex
+++ lib/gems_web/live/user_login_live.ex
@@ -9,13 +9,7 @@ defmodule GEMSWeb.UserLoginLive do
           <header class="text-center">
             <h2 class="text-2xl mb-4">Log in to account</h2>
           </header>
-          <.form
-            for={@form}
-            id="login_form"
-            action={~p"/login"}
-            phx-update="ignore"
-            class="space-y-4"
-          >
+          <.form for={@form} id="login_form" action={~p"/login"} phx-update="ignore" class="space-y-4">
             <.input field={@form[:email]} type="email" label="Email" required />
             <.input field={@form[:password]} type="password" label="Password" required />
             <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
