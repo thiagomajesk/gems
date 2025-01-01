54a44c7d87a81fca9a7765dcd9549e2ec24c4312
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2025-01-01 19:49:59 -0300

    Fix profession page missing column

--- lib/gems_web/live/admin/database/resource_live/forms/profession_component.ex
+++ lib/gems_web/live/admin/database/resource_live/forms/profession_component.ex
@@ -24,24 +24,12 @@ defmodule GEMSWeb.Admin.Database.ResourceLive.Forms.ProfessionComponent do
         </div>
         <Forms.field_input type="textarea" field={f[:description]} label="Description" />
         <Forms.field_input type="text" field={f[:icon]} label="Icon" />
-        <Forms.field_input
-          type="select"
-          field={f[:type]}
-          label="Type"
-          options={@profession_type_options}
-        />
       </Forms.base_form>
     </div>
     """
   end
 
   def mount(socket) do
-    {:ok, assign(socket, profession_type_options: profession_type_options())}
-  end
-
-  defp profession_type_options() do
-    GEMS.World.Schema.Profession
-    |> Ecto.Enum.mappings(:type)
-    |> Enum.map(fn {k, v} -> {Recase.to_title(v), k} end)
+    {:ok, socket}
   end
 end
