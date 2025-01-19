39820604d54052aaa67a861f558ad45cb22cc9ae
thiagomajesk <13930952+thiagomajesk@users.noreply.github.com>
2025-01-19 15:16:00 -0300

    Add support for plugin hooks

--- lib/gems_lua/manager.ex
+++ lib/gems_lua/manager.ex
@@ -9,10 +9,41 @@ defmodule GEMSLua.Manager do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def attach_hook(name, callback, lua) do
+    GenServer.cast(__MODULE__, {:attach_hook, name, callback, lua})
+  end
+
+  def trigger_hook(name, args \\ []) do
+    # Hook calls need to be synchronous otherwise the calling process might terminate
+    # the linked server and the hook will never be triggered (eg running mix tasks).
+    GenServer.call(__MODULE__, {:trigger_hook, name, args})
+  end
+
   @impl true
   def init(:ok) do
     paths = [@utils, GEMS.data_path()]
-    {:ok, %{lua: initialize_sandbox(paths)}}
+    {:ok, %{hooks: %{}, lua: initialize_sandbox(paths)}}
+  end
+
+  @impl true
+  def handle_cast({:attach_hook, name, callback, lua}, state) do
+    keys_path = [:hooks, Access.key(name, [])]
+    {:noreply, put_in(state, keys_path, {callback, lua})}
+  end
+
+  @impl true
+  def handle_call({:trigger_hook, name, args}, _from, state) do
+    case get_in(state, [:hooks, name]) do
+      nil ->
+        {:reply, :noop, state}
+
+      {callback, lua} ->
+        # For now we just use the caller's lua state instead of the server's
+        # state because hooks should be stateless (ie: not affect server's state).
+        {result, _lua} = Lua.call_function!(lua, callback, args)
+
+        {:reply, result, state}
+    end
   end
 
   defp initialize_sandbox(paths) do
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,3 +2,6 @@ alias GEMS.Seeder
 
 # Default admin user
 Seeder.create_admin("123123123")
+
+# Call the on seeds hook
+GEMSLua.Manager.trigger_hook(:on_seeds)
